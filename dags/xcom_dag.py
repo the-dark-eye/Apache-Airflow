ea40fd631a3ea50f59d0d2ffc4b8b573925834f4
the-dark-eye <nilesh.kala@konverge.ai>
2022-06-01 22:29:47 +0530

    XCOMs

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -11,18 +11,26 @@ default_args = {
     'start_date': datetime(2020, 1, 1)
 }
 
-def _training_model():
+def _training_model(ti):
     accuracy = uniform(0.1, 10.0)
     print(f'model\'s accuracy: {accuracy}')
+    ti.xcom_push(key='model_accuracy', value=accuracy)
 
-def _choose_best_model():
+def _choose_best_model(ti):
     print('choose best model')
+    accuracies = ti.xcom_pull(key='model_accuracy', task_ids=[
+        'processing_tasks.training_model_a',
+        'processing_tasks.training_model_b',
+        'processing_tasks.training_model_c',
+    ])
+    print(accuracies)
 
 with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
     downloading_data = BashOperator(
         task_id='downloading_data',
-        bash_command='sleep 3'
+        bash_command='sleep 3',
+        do_xcom_push=False
     )
 
     with TaskGroup('processing_tasks') as processing_tasks:
