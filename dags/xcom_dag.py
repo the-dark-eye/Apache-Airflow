cc5e54b334c09b0a698fac8a4884be08cb039c5f
the-dark-eye <nilesh.kala@konverge.ai>
2022-06-02 03:03:13 +0530

    Trigger Rules

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,11 +1,14 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.subdag import SubDagOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.operators.dummy import DummyOperator
 
 from random import uniform
 from datetime import datetime
+
+from numpy import choose
 
 default_args = {
     'start_date': datetime(2020, 1, 1)
@@ -23,7 +26,10 @@ def _choose_best_model(ti):
         'processing_tasks.training_model_b',
         'processing_tasks.training_model_c',
     ])
-    print(accuracies)
+    for accuracy in accuracies:
+        if accuracy > 8:
+            return 'accurate'
+    return 'inaccurate'
 
 with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
@@ -49,9 +55,23 @@ with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catc
             python_callable=_training_model
         )
 
-    choose_model = PythonOperator(
-        task_id='task_4',
+    choose_model = BranchPythonOperator(
+        task_id='choose_model',
         python_callable=_choose_best_model
     )
+    
+    accurate = DummyOperator(
+        task_id='accurate'
+    )
 
+    inaccurate = DummyOperator(
+        task_id='inaccurate'
+    )
+    
+    storing = DummyOperator(
+        task_id='storing',
+        trigger_rule='none_failed_or_skipped'
+    )
+    
     downloading_data >> processing_tasks >> choose_model
+    choose_model >> [accurate, inaccurate] >> storing
